c8b8c7748f547aa72de8e6321cd8f76aaa01ff74
Eduardo Xavier <49372451+Eduardo-XavierPaula@users.noreply.github.com>
2024-12-01 19:24:35 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   // Definir configurações de CORS
   app.enableCors({
     origin: 'https://frontend-omega-three-68.vercel.app', // Substitua pela URL do seu frontend
-    methods: 'GET, POST, PUT, DELETE, PATCH', // Métodos permitidos
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Métodos permitidos
     allowedHeaders: 'Content-Type, Authorization', // Cabeçalhos permitidos
   });
 
