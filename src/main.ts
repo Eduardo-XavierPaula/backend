e078cf01d95ac48ea3488f9b53ce19e8c62b6e75
Eduardo Xavier <49372451+Eduardo-XavierPaula@users.noreply.github.com>
2024-12-01 16:45:28 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ImATeapotException } from '@nestjs/common/exceptions';
+
+const whitelist = [
+  'https://frontend-omega-three-68.vercel.app',
+  'http://localhost:3000',
+];
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  // Definir configurações de CORS
-  app.enableCors({
-    origin: [
-      'https://frontend-omega-three-68.vercel.app',
-      'http://localhost:3000',
-    ], // Permitindo duas origens
-    methods: 'GET,POST,PUT,PATCH,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-    credentials: true,
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      origin: function (origin, callback) {
+        if (!origin) {
+          callback(null, true);
+          return;
+        }
+        if (
+          whitelist.includes(origin) || // Checks your whitelist
+          !!origin.match(/yourdomain\.com$/) // Overall check for your domain
+        ) {
+          console.log('allowed cors for:', origin);
+          callback(null, true);
+        } else {
+          console.log('blocked cors for:', origin);
+          callback(new ImATeapotException('Not allowed by CORS'), false);
+        }
+      },
+    },
   });
 
   await app.listen(3001); // Ou a porta que você está utilizando
