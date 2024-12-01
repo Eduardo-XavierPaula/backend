84107ce625c29c5b0c52e5d3c5f0fcfe745a4e19
Eduardo Xavier <49372451+Eduardo-XavierPaula@users.noreply.github.com>
2024-12-01 19:10:05 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,21 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const allowedOrigins = [
+    'http://localhost:3000', // Primeira URL permitida
+    'https://frontend-omega-three-68.vercel.app/', // Segunda URL permitida
+  ];
   // Definir configurações de CORS
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://frontend-omega-three-68.vercel.app',
-    ],
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true); // Permite a requisição
+      } else {
+        callback(new Error('Origin not allowed by CORS')); // Rejeita a requisição
+      }
+    },
     methods: 'GET,POST,PUT,PATCH,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
   });
 
   await app.listen(3001); // Ou a porta que você está utilizando
